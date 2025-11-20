ec35c78af923e5455d7e9268d2bdc1f1cba6c3bf
bardentdk <djebarlen.tambon@gmail.com>
2025-11-20 14:32:51 +0400

    Responsivisme revisité sur le top de la page index

--- resources/js/Pages/BilanMPI/Index.vue
+++ resources/js/Pages/BilanMPI/Index.vue
@@ -25,7 +25,7 @@ const formatDate = (date) => {
         <div class="max-w-7xl mx-auto">
             <!-- Header -->
             <div class="bg-white rounded-2xl shadow-xl p-8 mb-6">
-                <div class="flex items-center justify-between">
+                <div class="flex flex-col gap-y-10 sm:gap-none sm:flex-row ml:flex-row xl:flex-row items-start sm:items-center justify-between">
                     <div>
                         <h1 class="text-3xl font-bold text-gray-900 mb-2">
                             Bilans MPI Phase 1
@@ -54,10 +54,10 @@ const formatDate = (date) => {
                 <div 
                     v-for="bilan in bilans.data" 
                     :key="bilan.id"
-                    class="bg-white rounded-xl shadow-lg hover:shadow-2xl transition-all duration-200 overflow-hidden"
+                    class="bg-white rounded-xl shadow-lg hover:shadow-2xl transition-all duration-200 overflow-hidden "
                 >
                     <div class="p-6">
-                        <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+                        <div class="flex flex-col md:flex-row md:items-center md:justify-between gap-4 ">
                             <div class="flex-1">
                                 <div class="flex items-center mb-2">
                                     <div class="w-12 h-12 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-lg flex items-center justify-center mr-4">
@@ -124,8 +124,8 @@ const formatDate = (date) => {
             </div>
 
             <!-- Message si aucun bilan -->
-            <div v-else class="bg-white rounded-xl shadow-lg p-12 text-center">
-                <svg class="w-20 h-20 mx-auto text-gray-400 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <div v-else class="bg-white rounded-xl shadow-lg p-12 text-start sm:text-center ">
+                <svg class="w-20 h-20 mx-0 sm:mx-auto text-indigo-400 mb-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                 </svg>
                 <h3 class="text-xl font-semibold text-gray-700 mb-2">
